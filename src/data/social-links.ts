b6bd59dd12459caf0ffcaab1bafaee9be93d5b93
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-12-07 11:41:44 -0500

    feat: add social links with GitHub and LinkedIn icons

---
+++ src/data/social-links.ts
@@ -0,0 +1,18 @@
+import GithubIcon from '../assets/social/github.webp';
+import LinkedinIcon from '../assets/social/linkedin.webp';
+import type { SocialLink } from '../types/social-links';
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    icon: GithubIcon,
+    title: 'GitHub',
+    description: 'Alex-Huaracha',
+    href: 'https://github.com/Alex-Huaracha',
+  },
+  {
+    icon: LinkedinIcon,
+    title: 'LinkedIn',
+    description: 'alex-huaracha',
+    href: 'https://www.linkedin.com/in/alex-huaracha/',
+  },
+];
